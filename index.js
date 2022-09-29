f1f090d8fbac8ae6ce1031a9a30afe169c0bef8f
Rahul-Agarwal2002 <rahulagarwala2002dev@gmail.com>
2022-09-29 15:26:09 +0530

    Adding the database and additional compose route. Checkpoint

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ const bodyParser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose")
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-let posts = [];
+// let posts = [];
+mongoose.connect('mongodb://localhost:27017/blogDB')
+const postSchema = {
+  name : {type:String},
+  post : {type: String}
+}
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const Post = mongoose.model("Post", postSchema);
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get("/", (req, res) => {
-  res.render("home", { homepagecontent: homeStartingContent, posts: posts});
+  // res.render("home", { homepagecontent: homeStartingContent, posts: defaultPosts});
   // console.log(posts);
+  Post.find({}, function(err, foundItems){
+    res.render("home", { homepagecontent: homeStartingContent, posts: foundItems});  })
 });
 
 app.get("/about", (req, res) => {
@@ -36,7 +46,8 @@ app.get("/compose", (req, res) => {
 // express routing parameters is a very powerful tool to have in the arsenal. 
 app.get("/posts/:name", function(req,res){
   let requestedTitle = _.lowerCase(req.params.name);
-  posts.forEach((element)=>{
+  Post.find({}, function(err, foundItems){
+    foundItems.forEach(element => {
       let storedTitle = _.lowerCase(element.name);
       if(storedTitle === requestedTitle){
         res.render("post", {
@@ -46,7 +57,10 @@ app.get("/posts/:name", function(req,res){
         })  
 
     }
-  })
+    });
+     })
+
+
 
   
   
@@ -54,12 +68,16 @@ app.get("/posts/:name", function(req,res){
    
 
 app.post("/compose", (req, res) => {
-  var newCompose = {
+  let exists = false;
+  
+  const itemN = new Post({
     name: req.body.name,
-    post: req.body.post,
-  };
-  posts.push(newCompose);
+    post: req.body.post
+  })
+  defaultPosts.push(itemN);
+  itemN.save();
   res.redirect("/");
+
 });
 
 app.listen(3000, () => {
